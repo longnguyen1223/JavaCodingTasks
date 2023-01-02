b49ddf027e6818cbc00e483f9eb8cfcbd492bb1e
Brian <longnguyen.avt@gmail.com>
2023-01-02 16:47:57 -0600

    Task1 completed

---
+++ src/week2/Task1.java
@@ -0,0 +1,21 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        swap_Numbers();
+    }
+    public static void swap_Numbers(){// x=1 y=2
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter x: ");
+        int x=sc.nextInt();
+        System.out.println("enter y: ");
+        int y=sc.nextInt();
+        x= x+y; //x=1+2;
+        y=x-y;//y=3-2;
+        x=x-y;   //x=3-1;
+        System.out.println("after swap now x= "+x+", y= "+y);
+
+    }
+}
